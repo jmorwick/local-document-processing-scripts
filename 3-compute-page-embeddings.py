99146f6fd579b7e1df98342dcf36d15bf463e77d
jmorwick <jbmorwick@gmail.com>
2026-06-23 18:43:37 -0500

    fixed bug with encoding and cleaned up code

--- 3-compute-page-embeddings.py
+++ 3-compute-page-embeddings.py
@@ -9,6 +9,8 @@ import PIL
 import json
 import sys
 
+
+## set up params
 if len(sys.argv) < 4:
    print("usage: python script pdf_list_cid ocr_model embedding_model")
 
@@ -16,16 +18,17 @@ pdf_list_cid = sys.argv[1]
 ocr_model_name = sys.argv[2]
 embedding_model_name = sys.argv[3]
 
-
 print('computing embeddings with',embedding_model_name,'using OCR from',ocr_model_name)
 
-embedding_model = SentenceTransformer(embedding_model_name)   
 
-
-
+## set up cidni resources
 ds = FBDS('./cidnidb')
 kds = PFBDS('./cidnidb', levels=0)
 ks = IMKS(kds)
+
+
+## set up script resources
+embedding_model = SentenceTransformer(embedding_model_name)   
 
 def embed_text(text: str) -> np.ndarray:
     return embedding_model.encode(
@@ -34,21 +37,21 @@ def embed_text(text: str) -> np.ndarray:
             )
 
 
-document_cids = ds.recall(ds.decode(pdf_list_cid)).decode().split('\n')
+## process 
+document_cids = ds.recall(ds.decode(pdf_list_cid)).decode().strip().split('\n')
 print('Total documents to process:',len(document_cids))
 processed = 0
-for line in ds.recall(ds.decode(pdf_list_cid)).decode().split('\n'):
+for cid in document_cids:
+        cid = cid.strip()
         processed += 1
-        line = line.strip() 
-        if not line: continue
-        cid = ds.decode(line)   
+        if not cid: continue  
         
-        print('\n\n processing pdf: ', line)
+        print(f"\n\n processing pdf: {cid}")
         print(f"Progress: {(processed/len(document_cids)):.2%}\n\n")
         
-        for _, prop, page_cid in ks.inquire(subject=ds.encode(cid)):
+        for _, prop, page_cid in ks.inquire(subject=cid):
             if prop != 'CONTAINS': continue
-            kid, _ = ks.believe(ds.encode(cid), prop, page_cid)
+            kid, _ = ks.believe(cid, prop, page_cid)
             completed_models = set()
             _, _, pagenumber = list(ks.inquire(subject=ds.encode(kid), property='PAGE'))[0]
             print("STARTING Embedding computation FOR PAGE: ", pagenumber,'    Page CID:',page_cid)
@@ -58,15 +61,16 @@ for line in ds.recall(ds.decode(pdf_list_cid)).decode().split('\n'):
                 for _, _, cmodel in ks.inquire(subject=ds.encode(okid), property='MODEL'):
                     completed_models.add(cmodel)
                     print("** already computed: ",cmodel)
-
             if embedding_model_name in completed_models:
                 print('already complete for',embedding_model_name,'with',page_cid)
                 continue
-            
+           
             ocrtext = None
             for _, _, ocr_cid in ks.inquire(subject=page_cid, property='OCR'):
                 okid, _ = ks.believe(page_cid, 'OCR', ocr_cid)
+                print('found ocr...')
                 for _, _, cmodel in ks.inquire(subject=ds.encode(okid), property='MODEL'):
+                    print('found model: ',cmodel)
                     if ocr_model_name == cmodel:
                         ocrtext = ds.recall_text(ocr_cid)
             
